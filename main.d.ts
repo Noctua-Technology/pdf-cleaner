88e5f4a682052e2bcc4ee64248401cd68aecd250
deebloo <deebloo@proton.me>
2025-09-22 10:21:25 -0400

    fix type definitions

--- main.d.ts
+++ main.d.ts
@@ -1,12 +1,16 @@
 type RemoveText = (input: Uint8Array) => Promise<Uint8Array>;
+
+type LeaveOnlyText = (input: Uint8Array) => Promise<Uint8Array>;
+
 type FilterOperations = (
   input: Uint8Array,
   operationsToRemove: string[]
 ) => Promise<Uint8Array>;
 
 interface Cleaner {
-  removeText: RemoveText;
   filterOperations: FilterOperations;
+  leaveOnlyText: LeaveOnlyText;
+  removeText: RemoveText;
 }
 
 export function cleaner(): Promise<Cleaner>;
